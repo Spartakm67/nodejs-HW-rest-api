e98b7a0c80b163d813cb856d335278968d9eb6f2
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-05-10 17:47:06 +0300

    Try catch Added

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,29 +1,39 @@
-const express = require('express')
+const express = require('express');
 
 const contacts = require("../../models/contacts");
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  const result = await contacts.listContacts();
-  res.json(result);
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  }
+  catch (error) {
+    res.status(500).json({
+      message: "Server error"
+    })
+  }
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  const result = await contacts.getContactById();
-  res.json(result)
-})
+// router.get('/:contactId', async (req, res, next) => {
+//   const result = await contacts.getContactById(req.params.contactId);
+//   res.json(result)
+// })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.post('/', async (req, res, next) => {
+//   const newContact = await contacts.addContact(req.body);
+//   res.json(newContact);
+// });
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.delete('/:contactId', async (req, res, next) => {
+//   const result = await contacts.removeContact(req.params.contactId);
+//   res.json(result);
+// });
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.put('/:contactId', async (req, res, next) => {
+//   const result = await contacts.updateContact(req.params.contactId, req.body);
+//   res.json(result);
+// });
 
-module.exports = router
+module.exports = router;
