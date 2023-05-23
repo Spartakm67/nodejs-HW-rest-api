99520c950ae672e5167d79ebd544ffeb837a76e0
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-05-23 23:55:58 +0300

    HttpError Updated

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,4 +1,17 @@
-const HttpError = (status, message) => {
+const errorMessageList = {
+    '400': 'Bad Request',
+    '401': 'Not authorized',
+    '403': 'Forbidden',
+    '404': 'Not Found',
+    '405': 'Method Not Allowed',
+    '409': 'Conflict',
+    '500': 'Internal Server Error',
+    '503': 'Service Unavailable',
+    '504': 'Gateway Timeout'
+}
+
+
+const HttpError = (status, message = errorMessageList[status]) => {
     const error = new Error(message);
     error.status = status;
     return error;
--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -5,28 +5,19 @@ const { HttpError } = require("../helpers");
 const authenticate = async (req, res, next) => {
     const { authorization = "" } = req.headers;
     const [bearer, token] = authorization.split(" ");
-    // if (!bearer ||!token) {
-    //     return res.status(401).json({ message: "Unauthorized" });
-    // }
-    // try {
-    //     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    //     req.user = decoded;
-    //     next();
-    // } catch (err) {
-    //     return res.status(401).json({ message: "Unauthorized" });
-    // }
+    
     if (bearer !== "Bearer") {
-        next(HttpError(401, { message: "Not authorized" }));
+        next(HttpError(401));
     }
     try {
         const {id} = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findById(id);
         if (!user) {
-            next(HttpError(401, { message: "Not authorized" }));
+            next(HttpError(401));
         }
         next();
     } catch { 
-        next(HttpError(401, { message: "Not authorized" }));
+        next(HttpError(401));
         }
 };
 
