7d4f46f67e01d2c41f7b2b08f2829372e62d84aa
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-06-03 22:15:30 +0300

    Create auth-login.test.js

---
+++ controllers/auth/auth-login.test.js
@@ -0,0 +1,15 @@
+/*
+Unit tests for the login controller if the response is successful:
+1.The response should have a status code of 200.
+2.The response should include a token.
+3.The response should include a user object with two fields: "email" and "subscription", both of type String.
+ */
+
+// const login = require("./auth-login");
+
+// describe("the response should have a status code of 200", () => {
+
+// test()
+
+
+// });
